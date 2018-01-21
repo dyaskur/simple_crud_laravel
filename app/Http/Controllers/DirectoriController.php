f6f13339e35bd7f1398b96a8c9e6a33bde439a49
dyaskur <dyaskur@gmail.com>
2018-01-22 00:21:09 +0700

    directori model

---
+++ app/Http/Controllers/DirectoriController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\directori;
+use Illuminate\Http\Request;
+
+class DirectoriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\directori  $directori
+     * @return \Illuminate\Http\Response
+     */
+    public function show(directori $directori)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\directori  $directori
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(directori $directori)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\directori  $directori
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, directori $directori)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\directori  $directori
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(directori $directori)
+    {
+        //
+    }
+}
